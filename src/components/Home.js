cc0aaf8cf94afb677bae0eba29a27cfa2bac5cd4
Apoorva N <apn@adobe.com>
2023-03-20 23:24:30 +0530

    comment configure section

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,9 +20,9 @@ export default function Home() {
           Add a birthday
         </Link>
 
-        <Link to="/configure-emails" className="menu-item">
+        {/* <Link to="/configure-emails" className="menu-item">
           Configure reminders
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
